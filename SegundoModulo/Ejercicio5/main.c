abf6b67b73b7a3e28ea5d54c41d37a586af67719
Julian Fernandez <gonna1560@gmail.com>
2025-03-28 11:02:59 -0300

    ej 5 added

--- SegundoModulo/Ejercicio5/main.c
+++ SegundoModulo/Ejercicio5/main.c
@@ -10,8 +10,38 @@ solicitada en tiempo de ejecución.
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
-{
-    printf("Hello world!\n");
+int main() {
+    int *vp[20],n;
+
+    printf("Ingrese n\n");
+    scanf("%d",&n);
+
+    //Crear variables dinamicas en cada valor del vector
+    for(int i=0; i<n; i++) {
+        vp[i] = (int *) malloc(sizeof(int));
+        scanf("%d",vp[i]);
+
+    }
+
+    //Mostrar
+    printf("Todos\n");
+    for(int i=0; i<n; i++) {
+        printf("%d\n",*vp[i]);
+    }
+
+    //Mostrar Positivos
+    printf("Positivos\n");
+    for(int i=0; i<n; i++) {
+        if(*vp[i]>0) {
+          printf("%d\n",*vp[i]);
+        }
+    }
+
+    //Liberar memoria dinamica
+    for(int i=0; i<n; i++) {
+        free(vp[i]);
+    }
+
+
     return 0;
 }
