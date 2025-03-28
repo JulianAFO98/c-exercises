8b209f8542724b935c2810bf4c7e560cb1057370
Julian Fernandez <gonna1560@gmail.com>
2025-03-28 19:55:35 -0300

    algunos cambios y readme

--- PrimerModulo/ejercicios/Ejercicio30/main.c
+++ PrimerModulo/ejercicios/Ejercicio30/main.c
@@ -1,85 +1,92 @@
 /*
-Desarrollar un programa que lea y procese un archivo de texto generado por un radar vigía.
-Cada línea contiene patente de un vehículo, velocidad registrada, velocidad máxima permitida,
-fecha y hora de la medición. Cada dato está separado por un espacio. El programa debe
-generar un archivo binario con los registros donde la velocidad registrada supere en más de un
-20% a la velocidad máxima. Cada registro del archivo binario contiene los mismos datos que el
+Desarrollar un programa que lea y procese un archivo de texto generado por un radar vigï¿½a.
+Cada lï¿½nea contiene patente de un vehï¿½culo, velocidad registrada, velocidad mï¿½xima permitida,
+fecha y hora de la mediciï¿½n. Cada dato estï¿½ separado por un espacio. El programa debe
+generar un archivo binario con los registros donde la velocidad registrada supere en mï¿½s de un
+20% a la velocidad mï¿½xima. Cada registro del archivo binario contiene los mismos datos que el
 archivo de texto. Mostrar, al final del proceso, la cantidad de mediciones procesadas y la
 cantidad de registros generados en el archivo binario
 */
 
 #include <stdio.h>
 #include <stdlib.h>
-typedef struct {
+typedef struct
+{
     char patente[7];
     char hora[6];
     char fecha[11];
     int velReg;
     int velPermitida;
-}  Treg;
+} Treg;
 void generarBinario();
 void mostrarBinario();
 
-int main() {
+int main()
+{
     generarBinario();
     mostrarBinario();
     return 0;
 }
 
-
-
-void mostrarBinario() {
+void mostrarBinario()
+{
 
     Treg registro;
 
-    FILE * archBin = fopen("datosBinario.dat","r");
+    FILE *archBin = fopen("datosBinario.dat", "rb");
 
-    if(archBin == NULL) {
+    if (archBin == NULL)
+    {
         printf("Error al abrir algun archivo.\n");
         return;
-    } else {
+    }
+    else
+    {
 
-        fread(&registro,sizeof(Treg),1,archBin);
-        while(!feof(archBin)) {
-            printf("%s %s %s %d %d\n",registro.patente,registro.fecha,registro.hora,registro.velPermitida,registro.velReg);
-            fread(&registro,sizeof(Treg),1,archBin);
+        fread(&registro, sizeof(Treg), 1, archBin);
+        while (!feof(archBin))
+        {
+            printf("%s %s %s %d %d\n", registro.patente, registro.fecha, registro.hora, registro.velPermitida, registro.velReg);
+            fread(&registro, sizeof(Treg), 1, archBin);
         }
-
 
         fclose(archBin);
-
     }
-
 }
 
+void generarBinario()
+{
 
-void generarBinario() { // PREGUNTAR
-
-    char aux;
     Treg registro;
-    unsigned int totalMed=0,infracciones=0;
+    unsigned int totalMed = 0, infracciones = 0;
 
-    FILE * archRadar = fopen("datosRadar.txt","r");
-    FILE * archBin = fopen("datosBinario.dat","wb");
+    FILE *archRadar = fopen("datosRadar.txt", "r");
+    FILE *archBin = fopen("datosBinario.dat", "wb");
 
-    if(archRadar == NULL || archBin == NULL) {
+    if (archRadar == NULL)
+    {
+        printf("fallo1.\n");
+    }
+
+    if (archRadar == NULL || archBin == NULL)
+    {
         printf("Error al abrir algun archivo.\n");
-    } else {
-        while(fscanf(archRadar,"%s%d%d%c%s%c%s",registro.patente,&registro.velReg,&registro.velPermitida,&aux,registro.fecha,&aux,registro.hora)== 7) {
-            if((registro.velPermitida-registro.velReg)>registro.velPermitida*0.2) {
-                fwrite(&registro,sizeof(Treg),1,archBin);
-                infracciones+=1;
-
+    }
+    else
+    {
+        while (fscanf(archRadar, "%s%d%d %s %s", registro.patente, &registro.velReg, &registro.velPermitida, registro.fecha, registro.hora) == 5)
+        {
+            if ((registro.velPermitida - registro.velReg) > registro.velPermitida * 0.2)
+            {
+                fwrite(&registro, sizeof(Treg), 1, archBin);
+                infracciones += 1;
             }
-            totalMed+=1;
+            totalMed += 1;
         }
-        printf("Total mediciones: %d\n",totalMed);
-        printf("Infracciones: %d\n",infracciones);
+        printf("Total mediciones: %d\n", totalMed);
+        printf("Infracciones: %d\n", infracciones);
 
         fclose(archRadar);
         fclose(archBin);
-
-
     }
-
 }
---
+++ readme.md
@@ -0,0 +1,2 @@
+Ejercicios de C visto en la universidad(UNMDP)
+En cierto momento entre el segundo modulo y tercero cambia el formato de los ejercicios ya que se cambio de IDE
