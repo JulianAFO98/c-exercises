50228e576903467b9c11da14ffbef092a2b93453
Julian <gonna1560@gmail.com>
2025-05-29 21:03:23 -0300

    modificado 15

--- OctavoModulo/Ejercicio15/main.c
+++ OctavoModulo/Ejercicio15/main.c
@@ -15,6 +15,8 @@ void kruskal(int mat[MAX][MAX], int n);
 void inicializarVector(int vec[MAX],int n);
 int sonTodosIguales(int vec[MAX],int n);
 void actualizarValores(int vec[MAX],int n,int vi,int vj);
+void prim(int mat[MAX][MAX], int n,int inicial);
+
 
 int main()
 {
@@ -22,8 +24,53 @@ int main()
     llenarMatriz(mat, &n);
     mostrarMatriz(mat, n);
     kruskal(mat,n);
+    prim(mat,n,3);
     return 0;
 }
+
+int hayaCeros(int v[MAX],int n){
+    int i=0;
+    while(i<n && v[i]!=0){
+        i++;
+    }
+    return i<n;
+}
+
+
+void prim(int mat[MAX][MAX], int n,int inicial){
+    int U[MAX]={0},minimo,costoTotal=0,k=0;
+    regCamino vecCamino[MAX];
+    U[inicial]=1;
+    while(hayaCeros(U,n)){
+       minimo=9999;
+       for(int i=0;i<n;i++){
+        if(U[i]==1){
+            for(int j=0;j<n;j++){
+                if((mat[i][j]!=0 && mat[i][j]<minimo) && U[j]==0){
+                   minimo=mat[i][j];
+                   vecCamino[k].i=i;
+                   vecCamino[k].j=j;
+                }
+            }
+        }
+    }
+       U[vecCamino[k].j]=1;
+       costoTotal+=minimo;
+       k++;
+    }
+    printf("Costo total del camino:%d\n",costoTotal);
+    for (int i = 0; i < k; i++) {
+        printf("%d - %d\n", vecCamino[i].i + 1, vecCamino[i].j + 1);
+    }
+}
+
+
+
+
+
+
+
+
 
 void inicializarVector(int vec[MAX],int n){
     for(int i=0;i<n;i++){
